ea9acd6a7c80bab91178f6ad639ca2a2c3e2ec46
Nikolay-Kovalov <106543342+Nikolay-Kovalov@users.noreply.github.com>
2022-12-24 06:41:10 +0000

    Deploying to gh-pages from @ Nikolay-Kovalov/Workshop@f705611eecb92cbe6828e84834933d502ed130e9 🚀

---
+++ index.43fd8048.js
@@ -0,0 +1,2 @@
+fetch("https://api.themoviedb.org/3/movie/550?api_key=8234db11c162011590d14a3080848ed6",{headers:{"Api-Key":"8234db11c162011590d14a3080848ed6"}}).then((function(e){return e.json()})).then((function(e){return console.log(e)})).catch((function(e){return console.log(e)}));
+//# sourceMappingURL=index.43fd8048.js.map
---
+++ index.63569d08.js
@@ -0,0 +1,2 @@
+fetch("https://api.themoviedb.org/3/movie/550?api_key=8234db11c162011590d14a3080848ed6",{headers:{"Api-Key":"8234db11c162011590d14a3080848ed6"}}).then((e=>e.json())).then((e=>console.log(e))).catch((e=>console.log(e)));
+//# sourceMappingURL=index.63569d08.js.map
